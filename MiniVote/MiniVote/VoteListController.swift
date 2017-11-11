3c91810542214ff09b41b67e3f2360f1873f7be9
HanYoungsoo <gglife333@gmail.com>
2017-11-11 12:20:52 +0900

    add AF

--- MiniVote/MiniVote/VoteListController.swift
+++ MiniVote/MiniVote/VoteListController.swift
@@ -1,25 +1,27 @@
 //
-//  ViewController.swift
+//  MakeVoteViewContoller.swift
 //  MiniVote
 //
 //  Created by HanYoungsoo on 2017. 11. 11..
 //  Copyright © 2017년 YoungsooHan. All rights reserved.
 //
 
+import Foundation
+
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class VoteListController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 }
-
 
--- MiniVote/MiniVote/VoteResultController.swift
+++ MiniVote/MiniVote/VoteResultController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  VoteResult.swift
 //  MiniVote
 //
 //  Created by HanYoungsoo on 2017. 11. 11..
 //  Copyright © 2017년 YoungsooHan. All rights reserved.
 //
 
+import Foundation
+
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class VoteResultController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 }
-
-
