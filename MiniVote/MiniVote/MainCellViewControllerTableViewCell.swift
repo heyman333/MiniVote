193937b8993ed614dbe1c3d61da731c64dedec02
hyunahprincess <aare1213@gmail.com>
2017-11-12 17:52:04 +0900

    화면구성1/현아

---
+++ MiniVote/MiniVote/MainCellViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainCellViewControllerTableViewCell.swift
+//  MiniVote
+//
+//  Created by Hyunah on 2017. 11. 11..
+//  Copyright © 2017년 YoungsooHan. All rights reserved.
+//
+
+import UIKit
+
+class MainCellViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
